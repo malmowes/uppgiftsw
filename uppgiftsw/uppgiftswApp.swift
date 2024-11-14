1ee669bbba8405663a02d6eb5e7a8c1328e4ded7
wes <malmowes@hotmail.com>
2024-11-14 21:59:25 +0100

    Initial Commit

---
+++ uppgiftsw/uppgiftswApp.swift
@@ -0,0 +1,17 @@
+//
+//  uppgiftswApp.swift
+//  uppgiftsw
+//
+//  Created by applwes on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct uppgiftswApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
